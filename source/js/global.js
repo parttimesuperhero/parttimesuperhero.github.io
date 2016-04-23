2ea5b3fe73124dea777d61e0a5f2e0177a0dd71c
Matt <matt@Matts-MBP.home>
2016-04-23 10:48:30 +0100

    Testing Testing

--- source/js/global.js
+++ source/js/global.js
@@ -1,1 +1,11 @@
-alert("helloo");
+var $ = require('jquery');
+
+(function() {
+    var test = [1,2,3,4,5,6,8];
+
+    var sum = test.reduce( (a,b) => a + b );
+
+    $('h1').css({
+        'color': '#ff0000'
+    }).text(sum);
+})();
